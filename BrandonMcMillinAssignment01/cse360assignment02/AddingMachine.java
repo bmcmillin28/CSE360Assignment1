86ea61b5831175edbf14cc17ff8339167388ff8c
Brandon McMillin <bmcmillin@asu.edu>
2020-10-05 23:11:14 -0700

    initial check in of adding machine

---
+++ BrandonMcMillinAssignment01/cse360assignment02/AddingMachine.java
@@ -0,0 +1,34 @@
+package cse360assignment02;
+
+public class AddingMachine {
+	private int total;
+	
+	public AddingMachine() {
+		total = 0; // not needed - included for clarity
+	}
+	
+	//gets total
+	public int getTotal() {
+		return 0;
+	}
+	
+	//adds entered value
+	public void add(int value) {
+		
+	}
+	
+	//subtracts entered value
+	public void subtract(int value) {
+		
+	}
+	
+	//returns a string output
+	public String toString() {
+		return "";
+	}
+	
+	//clears data
+	public void clear() {
+		
+	}
+}
